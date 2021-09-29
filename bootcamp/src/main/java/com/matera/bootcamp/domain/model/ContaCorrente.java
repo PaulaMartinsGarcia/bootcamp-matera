f8c4bf67096ee55b8d79cc9e5fdb9dc9897ca378
PaulaMartinsGarcia <paulastsmartins@globo.com>
2021-09-28 21:03:23 -0300

    tabelas banco de dados

--- bootcamp/src/main/java/com/matera/bootcamp/domain/model/ContaCorrente.java
+++ bootcamp/src/main/java/com/matera/bootcamp/domain/model/ContaCorrente.java
@@ -4,12 +4,18 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 	public class ContaCorrente {
 		
 		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		
 		@Column(nullable = false)
